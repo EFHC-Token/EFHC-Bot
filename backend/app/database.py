09b4d69ae49269174908b08ed01c8c8d8b90b60e
EFHC Project <efhc.energy.for.hymanity@gmail.com>
2025-08-15 21:30:46 +0300

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,1 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from app.config import settings
 
+DATABASE_URL = f"postgresql+psycopg2://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+Base = declarative_base()
